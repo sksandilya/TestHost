a5cdc43027942ff10a80efe776723dbcdd2b8931
ssingh756 <ssingh756@rku.ac.in>
2022-06-22 11:14:51 +0530

    hosted

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var bodyParser = require('body-parser')
 var router = require("./route")
 
 var app = express();
-app.use(router);
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.json())
+app.use('/api', router);
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("server started");
 })
-
-
